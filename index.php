65124a0edc6b94f2c5bdd0dcd3733635c0508235
David Dempsey <davidgdempsey@gmail.com>
2020-02-09 05:50:37 +0000

    committing old changes

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<html>
-</html> 
+<?php
+	require "header.php";
+?>
 
+	<main>
+		<div class="wrapper-main">
+			<section class="section-default">
+				<?php
+					if (isset($_SESSION['userId'])) {
+						echo '<p class="login-status">You are logged in!</p>';
+						echo '<form action="upload.php" method="POST" enctype="multipart/form-data">
+							<input type="file" name="file">
+							<button type="submit" name="submit">UPLOAD</button>
+							</form>';
+					}
+					else {
+						echo '<p class="login-status">You are logged out!</p>';
+					}
+				?>
+			</section>
+		</div>	
+	</main>
 
+<?php
+	require "footer.php";
+?>
